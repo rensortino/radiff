e8fb9b1146870d6fc9a8e1b4863db5fd02e4922d
Renato Sortino <rensortino@gmail.com>
2023-11-06 02:56:43 +0100

    Updated datasets

--- datasets/rg_masks.py
+++ datasets/rg_masks.py
@@ -22,35 +22,13 @@ warnings.simplefilter("ignore", category=VerifyWarning)
 from utils.image import mask_to_rgb
 
 
-class MaskOnlyDataset(Dataset):
-    def __init__(self, data_dir, mask_paths, device):
-        super().__init__()
-        data_dir = Path(data_dir)
-        self.device = device
-        with open(mask_paths) as f:
-            self.mask_paths = f.read().splitlines()
-        self.mask_paths = [data_dir / p for p in self.mask_paths]
-
-    def __len__(self):
-        return len(self.mask_paths)
-
-    def __getitem__(self, idx):
-        mask_path = self.mask_paths[idx]
-        mask = fits.getdata(mask_path).astype(np.float32)
-        mask = torch.from_numpy(mask)
-        mask = mask.unsqueeze(0)
-        mask = TF.resize(mask, (128, 128))
-        assert (
-            mask.unique().max() <= 3
-        ), "Mask must have one of the following values: 0, 1, 2, or 3"
-
-        return mask
 
 
 class MaskRGDataset(RGDataset):
-    def __init__(self, data_dir, img_paths, img_size=128):
+    def __init__(self, data_dir, img_paths, img_size=128, mask_only=False):
         super().__init__(data_dir, img_paths, img_size)
         self.img_size = img_size
+        self.mask_only = mask_only
 
         self.mask_transforms = T.Compose(
             [
@@ -62,26 +40,13 @@ class MaskRGDataset(RGDataset):
             ]
         )
 
+
     def __getitem__(self, idx):
         image_path = self.img_paths[idx]
-        img = fits.getdata(image_path)
-        img = self.transforms(img)
-
-        # Random crop
-        # crop_size = self.img_size // 2
-        h_crop_size = random.sample(range(self.img_size // 4, self.img_size), 1)[0]
-        w_crop_size = random.sample(range(self.img_size // 4, self.img_size), 1)[0]
-        t, l, h, w = T.RandomCrop.get_params(
-            img, output_size=(h_crop_size, w_crop_size)
-        )
-        crop = TF.crop(img, t, l, h, w)
-        r = self.img_size - w - l
-        b = self.img_size - h - t
-        masked_image = TF.pad(crop, padding=[l, t, r, b], fill=0)
-
         ann_path = str(image_path).replace("imgs", "masks").replace(".fits", ".json")
         ann_dir = Path(ann_path).parent
         ann_path = ann_dir / f'mask_{ann_path.split("/")[-1]}'
+        
         with open(ann_path) as j:
             mask_info = json.load(j)
 
@@ -114,6 +79,12 @@ class MaskRGDataset(RGDataset):
             masks.append(mask)
 
         mask, _ = torch.max(torch.stack(masks), dim=0)
+
+        if self.mask_only:
+            return mask
+        
+        img = fits.getdata(image_path)
+        img = self.transforms(img)
 
         return img, mask, masked_image
 
